b0966ee97f5625dff53e08cb22ab29b4f8c3c4de
EliStillCantCode <n4thenknowsdawae442@gmail.com>
2022-04-24 15:37:23 +0100

    digit selector

---
+++ Digits Of Pi/Digits Of Pi/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Clear();
+var pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+string? digits = ("3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679").Length.ToString();
+
+var isConvertible = false;
+var rDigitsInt = 0;
+
+while (!isConvertible)
+{
+    Console.Clear();
+    Console.WriteLine("How many digits of Pi do you want? 1-100");
+    var requestedDigits = Console.ReadLine();
+
+    isConvertible = int.TryParse(requestedDigits, out rDigitsInt);
+}
